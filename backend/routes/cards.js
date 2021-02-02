e6368b8ae472507ca9deac49784133be8ea6003d
Alexsandr Chigarev <majestik13@mail.ru>
2021-02-02 13:49:48 +0300

    FIX 15

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -7,9 +7,9 @@ const {
 router.get('/cards', getCards);
 
 router.post('/cards', celebrate({
-  body: Joi.object().keys({
-    link: Joi.string().pattern(new RegExp('^(https?:\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})([/\\w\\W.-]*)#?$')),
-    name: Joi.string().min(2).max(30),
+  body: Joi.object().required().keys({
+    link: Joi.string().required().pattern(new RegExp('^(https?:\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})([/\\w\\W.-]*)#?$')),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), createCard);
 
